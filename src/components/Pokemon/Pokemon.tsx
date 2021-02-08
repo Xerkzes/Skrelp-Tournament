8db5c18d941be5e3aaaa5809e9bfe834c6302cfe
Xerkzes <>
2021-02-08 20:48:11 +0100

    added new trainers (TheLoon, Liljaka, TheTired) and load sprites in sprite file

--- src/components/Pokemon/Pokemon.tsx
+++ src/components/Pokemon/Pokemon.tsx
@@ -12,21 +12,140 @@ interface PokemonProps {
 function createPokemonUrl(pokemonName: string) {
   const name: string = pokemonName.toLowerCase();
 
-  if (name.includes("alola")) {
-    return name + "n";
-  }
-  if (name.includes("galar")) {
-    return name + "ian";
-  }
-  if (name === "meloetta-aria") {
-    return name.substring(0, 8);
+  if (name.includes("-alola")) {
+    return "-alola";
   }
 
-  if (name === "morpeko") {
-    return name + "-full-belly";
+  if (name.includes("-mega")) {
+    if (name.includes("-x")) {
+      return "-mega-x";
+    }
+    if (name.includes("-y")) {
+      return "-mega-y";
+    }
+    return "-mega";
   }
-  if (name === "minior-red-meteor") {
-    return "minior-meteor";
+
+  if (name.includes("-galar")) {
+    return "-galar";
+  }
+
+  // it's one of the Rotom forms
+  if (name.includes("rotom-")) {
+    if (name.includes("fan")) {
+      return "-fan";
+    }
+    if (name.includes("frost")) {
+      return "-frost";
+    }
+    if (name.includes("heat")) {
+      return "-heat";
+    }
+    if (name.includes("mow")) {
+      return "-mow";
+    }
+    if (name.includes("wash")) {
+      return "-wash";
+    }
+  }
+
+  // it's one of the Oricorio forms
+  if (name.includes("oricorio-")) {
+    if (name.includes("pau")) {
+      return "-pau";
+    }
+    if (name.includes("pom")) {
+      return "--pom-pom";
+    }
+    if (name.includes("sensu")) {
+      return "-sensu";
+    }
+  }
+
+  // it's one of the Indeedee forms
+  if (name.includes("indeedee-")) {
+    if (name.includes("-female")) {
+      return "-f";
+    }
+  }
+
+  // it's one of the Zygarde forms
+  if (name.includes("zygarde-")) {
+    if (name.includes("-10")) {
+      return "-10";
+    }
+    if (name.includes("-complete")) {
+      return "-complete";
+    }
+  }
+
+  if (name === "thundurus-therian") {
+    return "-therian";
+  }
+
+  return "";
+}
+
+function createPokemonForFetchDexNr(pokemonName: string) {
+  const name: string = pokemonName.toLowerCase();
+
+  if (name.includes("-alola")) {
+    return name.substring(0, name.length - 6);
+  }
+
+  if (name.includes("-mega")) {
+    if (name.includes("-x") || name.includes("-y")) {
+      return name.substring(0, name.length - 7);
+    }
+    return name.substring(0, name.length - 5);
+  }
+
+  if (name.includes("-galar")) {
+    return name.substring(0, name.length - 6);
+  }
+
+  if (name.includes("wormadam")) {
+    return "wormadam";
+  }
+
+  if (name.includes("toxtricity")) {
+    return "toxtricity";
+  }
+
+  if (name.includes("rotom-")) {
+    return "rotom";
+  }
+
+  if (name === "aegislash-shield" || name === "aegislash-blade") {
+    return "aegislash";
+  }
+
+  if (name.includes("meloetta-")) {
+    return "meloetta";
+  }
+
+  if (name.includes("oricorio-")) {
+    return "oricorio";
+  }
+
+  if (name.includes("gourgeist-")) {
+    return "gourgeist";
+  }
+
+  if (name.includes("indeedee-")) {
+    return "indeedee";
+  }
+
+  if (name.includes("zygarde-")) {
+    return "zygarde";
+  }
+
+  if (name.includes("minior-")) {
+    return "minior";
+  }
+
+  if (name.includes("thundurus")) {
+    return "thundurus";
   }
 
   return name;
@@ -43,15 +162,23 @@ export const Pokemon: React.FC<PokemonProps> = ({ pokemon }) => {
 
   useEffect(() => {
     async function loadPokemons() {
-      const fetchUrl =
+      // get data from API
+      const fetchDataUrl =
         "https://pokeapi.co/api/v2/pokemon/" + pokemon.name.toLowerCase();
-      const response = await fetch(fetchUrl);
+      const fetchDexNrUrl =
+        "https://pokeapi.co/api/v2/pokemon-species/" +
+        createPokemonForFetchDexNr(pokemon.name);
+
+      const response = await fetch(fetchDataUrl);
       const data = await response.json();
+      const response2 = await fetch(fetchDexNrUrl);
+      const dexNr = await response2.json();
 
-      const pokemonName = createPokemonUrl(pokemon.name);
-
-      // setImgUrl(data.sprites.front_default);
-      setImgUrl("https://img.pokemondb.net/artwork/" + pokemonName + ".jpg");
+      // add suffix if needed
+      const suffix = createPokemonUrl(pokemon.name);
+      // sets Sprite Url
+      setImgUrl("sprites/normal/" + dexNr.id + suffix + ".png");
+      // helpers/sprites/png/normal
 
       try {
         setHP(data.stats[0].base_stat);
@@ -70,17 +197,17 @@ export const Pokemon: React.FC<PokemonProps> = ({ pokemon }) => {
 
   return (
     <div className="pokemon-div">
-      <p
+      <div
         style={{ backgroundColor: typeColors[pokemon.type.toLowerCase()] }}
         className="pokemon-type"
       >
         {pokemon.type}
-      </p>
+      </div>
       <img className="pokemon-img" src={imgUrl} alt="img" />
-      <p className="pokemon-name">{pokemon.name}</p>
-      <p className="pokemon-stats">
+      <div className="pokemon-name">{pokemon.name}</div>
+      <div className="pokemon-stats">
         {hp} / {atk} / {def} / {spA} / {spD} / {spe}
-      </p>
+      </div>
     </div>
   );
 };
