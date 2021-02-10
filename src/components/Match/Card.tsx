f1852f9f5b3c539d6d6bb64c5a3615cf1933cf62
Xerkzes <>
2021-02-10 14:56:00 +0100

    made the Matches-Page load the data dynamical

---
+++ src/components/Match/Card.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+interface CardProps {
+  data: {
+    header: string;
+    matches: {
+      name1: string;
+      name2: string;
+      winner: string;
+    }[];
+  };
+}
+
+export const Card: React.FC<CardProps> = ({ data }) => {
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{data.header}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <div className="match-week">
+              {data.matches.map((match, idx) => {
+                return (
+                  <div className="match">
+                    <p
+                      className={
+                        "team team-left " +
+                        (match.winner === ""
+                          ? ""
+                          : match.winner === match.name1
+                          ? "match-won"
+                          : "match-lost")
+                      }
+                    >
+                      {match.name1}
+                    </p>
+                    <p className="vs">-</p>
+                    <p
+                      className={
+                        "team " +
+                        (match.winner === ""
+                          ? ""
+                          : match.winner === match.name2
+                          ? "match-won"
+                          : "match-lost")
+                      }
+                    >
+                      {match.name2}
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+};
--- src/components/Match/Match.tsx
+++ src/components/Match/Match.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import "./style.css";
-import Accordion from "@material-ui/core/Accordion";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MatchesData from "../../helpers/Match.json";
+import { Card } from "./Card";
 
 interface MatchProps {}
 
@@ -12,93 +9,11 @@ export const Matches: React.FC<MatchProps> = ({}) => {
   return (
     <div>
       <h1 className="header">Matches</h1>
-      <div className="matches-container">
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>Week1: (Feb 08-14)</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              <div className="match-week">
-                <div className="match">
-                  <p className="team team-left">RhydonMyDeek</p>
-                  <p className="vs">-</p>
-                  <p className="team">martypyds</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left">X3rkzes</p>
-                  <p className="vs">-</p>
-                  <p className="team">vaugsy17</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left">r3z3n15</p>
-                  <p className="vs">-</p>
-                  <p className="team">mcguineeplasto</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left">bumper09</p>
-                  <p className="vs">-</p>
-                  <p className="team">QuestionableSpecimen</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left">FatherGung</p>
-                  <p className="vs">-</p>
-                  <p className="team">Mannat</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left">Drxx</p>
-                  <p className="vs">-</p>
-                  <p className="team">vinomatic101</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left">runanbun_</p>
-                  <p className="vs">-</p>
-                  <p className="team">daddybeefcakes</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left">alienx3</p>
-                  <p className="vs">-</p>
-                  <p className="team">PapierJoe</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left">lefty8511</p>
-                  <p className="vs">-</p>
-                  <p className="team">ElementalTem</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left match-lost">TheLoooon</p>
-                  <p className="vs">-</p>
-                  <p className="team match-won">rossboss00</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left">TheTired</p>
-                  <p className="vs">-</p>
-                  <p className="team">ajstyles</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left">Professor_Mad_Lad</p>
-                  <p className="vs">-</p>
-                  <p className="team">Wufn1k</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left">xxsirsavagexx</p>
-                  <p className="vs">-</p>
-                  <p className="team">Liljaka</p>
-                </div>
-                <div className="match">
-                  <p className="team team-left">099percentbeast</p>
-                  <p className="vs">-</p>
-                  <p className="team">jmurphy725</p>
-                </div>
-              </div>
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-      </div>
+      <div className="matches-container"></div>
+
+      {MatchesData.map((data, idx) => {
+        return <Card key={idx} data={data} />;
+      })}
     </div>
   );
 };
