44215d292bbf6a17cc141f9db4db8c2ee7119ad9
Xerkzes <>
2021-02-11 10:58:07 +0100

    made small json with all the divisons

--- src/components/Rooster/Rooster.tsx
+++ src/components/Rooster/Rooster.tsx
@@ -29,119 +29,74 @@ interface Trainer {
   }[];
 }
 
+interface Division {
+  array: Trainer[];
+  name: string;
+}
+
 export const Rooster: React.FC<RoosterProps> = ({}) => {
+  const forceUpdate = React.useReducer(() => ({}), {})[1] as () => void;
+
   const [red, setRed] = useState<Trainer[]>([]);
-  const [green, setGreen] = useState<Trainer[]>([]);
   const [blue, setBlue] = useState<Trainer[]>([]);
+  const [green, setGreen] = useState<Trainer[]>([]);
   const [yellow, setYellow] = useState<Trainer[]>([]);
 
+  const divisions: Array<Division> = [
+    { array: red, name: "Red" },
+    { array: blue, name: "Blue" },
+    { array: green, name: "Green" },
+    { array: yellow, name: "Yellow" },
+  ];
+
   useEffect(() => {
-    const r: Trainer[] = [];
-    const b: Trainer[] = [];
-    const g: Trainer[] = [];
-    const y: Trainer[] = [];
     // sort Array by ASC of the team-number
     TrainerData.sort(sortByValue("team"));
 
     // puts the trainer into the their rooster
     TrainerData.forEach((trainer) => {
-      if (trainer.division == "red") {
-        r.push(trainer);
-      } else if (trainer.division == "blue") {
-        b.push(trainer);
-      } else if (trainer.division == "green") {
-        g.push(trainer);
-      } else if (trainer.division == "yellow") {
-        y.push(trainer);
-      }
+      divisions.filter((obj) => {
+        if (obj.name.toLocaleLowerCase() === trainer.division)
+          obj.array.push(trainer);
+      });
     });
 
-    setRed(r);
-    setBlue(b);
-    setGreen(g);
-    setYellow(y);
+    forceUpdate();
   }, []);
 
   return (
     <div>
       <h1 className="header">Divisions</h1>
-
       <div className="rooster-divisions-container">
-        <div className="rooster-division rooster-divison-red">
-          <h2>Red Division</h2>
-          <div className="rooster-teams">
-            {red.map((trainer, idx) => {
-              return (
-                <div key={idx} className="rooster-team">
-                  <p className="rooster-team-name">
-                    {trainer.team}: {trainer.name}
-                  </p>
-                  <div className="rooster-team-scores">
-                    <p className="rooster-team-games-won">{trainer.wins}</p>
-                    <p className="rooster-team-games-lost">{trainer.loses}</p>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-
-        <div className="rooster-division rooster-divison-blue">
-          <h2>Blue Division</h2>
-          <div className="rooster-teams">
-            {blue.map((trainer, idx) => {
-              return (
-                <div key={idx} className="rooster-team">
-                  <p className="rooster-team-name">
-                    {trainer.team}: {trainer.name}
-                  </p>
-                  <div className="rooster-team-scores">
-                    <p className="rooster-team-games-won">{trainer.wins}</p>
-                    <p className="rooster-team-games-lost">{trainer.loses}</p>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-
-        <div className="rooster-division rooster-divison-green">
-          <h2>Green Division</h2>
-          <div className="rooster-teams">
-            {green.map((trainer, idx) => {
-              return (
-                <div key={idx} className="rooster-team">
-                  <p className="rooster-team-name">
-                    {trainer.team}: {trainer.name}
-                  </p>
-                  <div className="rooster-team-scores">
-                    <p className="rooster-team-games-won">{trainer.wins}</p>
-                    <p className="rooster-team-games-lost">{trainer.loses}</p>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-
-        <div className="rooster-division rooster-divison-yellow">
-          <h2>Yellow Division</h2>
-          <div className="rooster-teams">
-            {yellow.map((trainer, idx) => {
-              return (
-                <div key={idx} className="rooster-team">
-                  <p className="rooster-team-name">
-                    {trainer.team}: {trainer.name}
-                  </p>
-                  <div className="rooster-team-scores">
-                    <p className="rooster-team-games-won">{trainer.wins}</p>
-                    <p className="rooster-team-games-lost">{trainer.loses}</p>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+        {divisions.map((obj, idx) => {
+          return (
+            <div
+              key={idx}
+              className={
+                "rooster-division rooster-divison-" + obj.name.toLowerCase()
+              }
+            >
+              <h2>{obj.name} Division</h2>
+              <div className="rooster-teams">
+                {obj.array.map((trainer, idx) => {
+                  return (
+                    <div key={idx} className="rooster-team">
+                      <p className="rooster-team-name">
+                        {trainer.team}: {trainer.name}
+                      </p>
+                      <div className="rooster-team-scores">
+                        <p className="rooster-team-games-won">{trainer.wins}</p>
+                        <p className="rooster-team-games-lost">
+                          {trainer.loses}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
