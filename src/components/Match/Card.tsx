b9c47689b95667dbfd0489655c5b457da4a5ca4e
Xerkzes <>
2021-02-15 10:09:51 +0100

    updated the new matches

--- src/components/Match/Card.tsx
+++ src/components/Match/Card.tsx
@@ -18,53 +18,51 @@ interface CardProps {
 
 export const Card: React.FC<CardProps> = ({ data }) => {
   return (
-    <div>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>{data.header}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            <div className="match-week">
-              {data.matches.map((match, idx) => {
-                return (
-                  <div className="match">
-                    <p
-                      className={
-                        "team team-left " +
-                        (match.winner === ""
-                          ? ""
-                          : match.winner === match.name1
-                          ? "match-won"
-                          : "match-lost")
-                      }
-                    >
-                      {match.name1}
-                    </p>
-                    <p className="vs">-</p>
-                    <p
-                      className={
-                        "team " +
-                        (match.winner === ""
-                          ? ""
-                          : match.winner === match.name2
-                          ? "match-won"
-                          : "match-lost")
-                      }
-                    >
-                      {match.name2}
-                    </p>
-                  </div>
-                );
-              })}
-            </div>
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    </div>
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography>{data.header}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography>
+          <div className="match-week">
+            {data.matches.map((match, idx) => {
+              return (
+                <div className="match">
+                  <p
+                    className={
+                      "team team-left " +
+                      (match.winner === ""
+                        ? ""
+                        : match.winner === match.name1
+                        ? "match-won"
+                        : "match-lost")
+                    }
+                  >
+                    {match.name1}
+                  </p>
+                  <p className="vs">-</p>
+                  <p
+                    className={
+                      "team " +
+                      (match.winner === ""
+                        ? ""
+                        : match.winner === match.name2
+                        ? "match-won"
+                        : "match-lost")
+                    }
+                  >
+                    {match.name2}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </Typography>
+      </AccordionDetails>
+    </Accordion>
   );
 };
